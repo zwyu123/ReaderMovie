b6b5ca00e9fd3cf223db72d1ebd11e03de6eaf1f
zwyu123 <zwyu2017@163.com>
2019-12-24 22:18:40 +0800

    交互反馈wx.showActionSheet

--- pages/posts/post-detail/post-detail.js
+++ pages/posts/post-detail/post-detail.js
@@ -45,5 +45,24 @@ Page({
       duration: 1000,
       icon: "success"
     })
+  },
+
+  onShareTap: function(event) {
+    var itemList = [
+      "分享给微信好友",
+      "分享到朋友圈",
+      "分享到QQ",
+      "分享到微博"
+    ]
+    wx.showActionSheet({
+      itemList: itemList,
+      itemColor:"#405f80",
+      success: function(res) {
+        wx.showModal({
+          title: "用户" + itemList[res.tapIndex],
+          content: "用户是否取消?" + res.cancel + "现在无法实现分享功能，什么时候能支持呢"
+        })
+      }
+    })
   }
 })
